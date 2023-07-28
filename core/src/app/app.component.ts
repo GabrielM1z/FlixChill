ca1797b5f50e56c5ec39b05ea668da9dd05bef0e
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2023-07-28 13:35:11 +0200

    debut du lien entre notre api et le front

--- core/src/app/app.component.ts
+++ core/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FlixChill';
+  apiResponse: any; // Variable pour stocker la réponse de l'API
+
+  constructor(private http: HttpClient) {}
+
+  envoyerRequete(): void {
+    const apiUrl = 'http://127.0.0.1:8000/';
+
+    this.http.get(apiUrl)
+      .subscribe(
+        (data: any) => {
+          // Mettre à jour la variable pour stocker la réponse de l'API
+          this.apiResponse = data;
+        },
+        (error: any) => {
+          console.error('Erreur lors de la requête AJAX :', error);
+        }
+      );
+  }
 }
