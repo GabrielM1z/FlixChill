171bdba8278d26fdd5a294424f3a7097533d0515
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2023-08-02 17:30:53 +0200

    fin seance

--- core/src/app/app.component.ts
+++ core/src/app/app.component.ts
@@ -1,6 +1,120 @@
+// import { Component } from '@angular/core';
+// import { ApiService } from './services/api.service';
+// import { HomeComponent } from './pages/home/home.component';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+  
+//   recommendation: any;
+//   backgroundTheme: any;
+//   lstWeather : any;
+//   lstGenre : any = ["Action", 'Aventure', 'Animation'];
+//   basePath : string = "https://image.tmdb.org/t/p/w185";
+
+
+//   constructor(private apiService: ApiService) {}
+
+//   ngOnInit() {
+//     console.log('ngOnInit called');
+
+//     this.apiService.getBackgroundTheme(encodeURIComponent('Mazatlán')).subscribe(
+//       (response) => {
+//         this.backgroundTheme = response.backgroundTheme[0];
+//         if(this.backgroundTheme == "dark_mode"){
+//           this.apiService.isBlackTheme = true;
+//         }else{
+//           this.apiService.isBlackTheme = false;
+//         }
+//         console.log(this.backgroundTheme)
+//       },
+//       (error) => {
+//         console.error('Error fetching recommendation:', error);
+//       }
+//     );
+
+//     this.apiService.getWeather().subscribe(
+//       (response) => {
+//         this.lstWeather = response;
+//         console.log(this.lstWeather)
+//       },
+//       (error) => {
+//         console.error('Error fetching recommendation:', error);
+//       }
+//     );
+    
+    
+//     this.apiService.getGenre().subscribe(
+//       (response) => {
+//         this.lstGenre = response;
+//         console.log(response);
+//       },
+//       (error) => {
+//         console.error('Error fetching recommendation:', error);
+//       }
+//     );
+
+//     this.apiService.getRecommendedByGender(this.lstGenre).subscribe(
+//       (response) => {
+
+//         console.log("final reponse", response)
+
+//       },
+//       (error) => {
+//         console.error('Error fetching recommendation:', error);
+//       }
+//     );
+//   }
+
+// }
+
+// /*
+//     this.apiService.getRecommendedByGender().subscribe(
+//       (response) => {
+
+//         console.log("final reponse", response)
+
+//       },
+//       (error) => {
+//         console.error('Error fetching recommendation:', error);
+//       }
+//     );*/
+    
+//    /*
+//    let a = this.getGenre();
+//    console.log("a=",a)
+//     this.getRecommendedByGender(a);
+//     */
+
+//   /*async getGenre() {
+//     let response;
+//     try {
+//       response = await this.apiService.getGenre().toPromise();
+
+//       this.lstGenre = response;
+//       console.log("eee",response)
+//     } catch (error) {
+//       console.error('Error fetching recommendation:', error);
+//       response = ""
+//     }
+//     return response;
+//   }
+
+//   async getRecommendedByGender(lstGenre: any) {
+//     try {
+//       console.log("c'est ici",lstGenre)
+//       let response = await this.apiService.getRecommendedByGender(lstGenre).toPromise();
+//       console.log("re", response);
+//     } catch (error) {
+//       console.error('Error fetching recommendation:', error);
+//     }
+//   }*/
 import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
 import { ApiService } from './services/api.service';
-import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -8,49 +122,51 @@ import { HomeComponent } from './pages/home/home.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   recommendation: any;
   backgroundTheme: any;
-  lstWeather : any;
-  basePath : string = "https://image.tmdb.org/t/p/w185";
+  lstWeather: any;
+  lstGenre: any;
+  lstMovieGenre: any;
+  basePath: string = "https://image.tmdb.org/t/p/w185";
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     console.log('ngOnInit called');
 
-    this.apiService.getBackgroundTheme(encodeURIComponent('Mazatlán')).subscribe(
-      (response) => {
-        this.backgroundTheme = response.backgroundTheme[0];
-        if(this.backgroundTheme == "dark_mode"){
+    const requests = [
+      this.apiService.getBackgroundTheme(encodeURIComponent('Prague')),
+      this.apiService.getWeather(),
+      this.apiService.getGenre()
+    ];
+
+    forkJoin(requests).subscribe(
+      (responses) => {
+        this.backgroundTheme = responses[0].backgroundTheme[0];
+        if (this.backgroundTheme === "dark_mode") {
           this.apiService.isBlackTheme = true;
-        }else{
+        } else {
           this.apiService.isBlackTheme = false;
         }
-        console.log(this.backgroundTheme)
-      },
-      (error) => {
-        console.error('Error fetching recommendation:', error);
-      }
-    );
+        console.log(this.backgroundTheme);
 
-    this.apiService.getWeather().subscribe(
-      (response) => {
-        this.lstWeather = [];
-        console.log(response)
-        for(let i = 0; i < response.length; i++){
-          this.lstWeather.push(response[i].poster_path)
-        }
-        console.log(this.lstWeather)
-      },
-      (error) => {
-        console.error('Error fetching recommendation:', error);
-      }
-    );
+        this.lstWeather = responses[1];
+        console.log(this.lstWeather);
 
-    this.apiService.getGenre().subscribe(
-      (response) => {
-        console.log(response)
+        this.lstGenre = responses[2];
+        console.log(this.lstGenre);
+
+        // Maintenant que les autres appels sont terminés, appelons getRecommendedByGender
+        this.apiService.getRecommendedByGender(this.lstGenre).subscribe(
+          (response) => {
+            this.lstMovieGenre = response
+            console.log("lstMovieGenre", response);
+          },
+          (error) => {
+            console.error('Error fetching recommendation:', error);
+          }
+        );
       },
       (error) => {
         console.error('Error fetching recommendation:', error);
--- core/src/app/services/api.service.ts
+++ core/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { forkJoin,Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ import { Observable } from 'rxjs';
 export class ApiService {
 
   isBlackTheme: boolean  = false;
+  lstReturn : any;
 
   private baseUrl = 'http://127.0.0.1:8000'; // Replace with the correct backend URL
 
@@ -26,7 +28,30 @@ export class ApiService {
   }
 
   getGenre(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/genre/`);
+    return this.http.get<any>(`${this.baseUrl}/genre`);
   }
+
+  getRecommendedByGender(lstGenre : any): Observable<any> {
+    //console.log("ok",lstGenre)
+    this.lstReturn = [];
+    for(let i = 0; i< lstGenre.length; i++){
+      //console.log("i=",i)
+      encodeURIComponent(lstGenre[i])
+      let a = this.http.get<any>(`${this.baseUrl}/recommendation/list/${encodeURIComponent(lstGenre[i])}`);
+      this.lstReturn.push(a)
+    }
+    //console.log("a",this.http.get<any>(`${this.baseUrl}/genre`))
+    return forkJoin(this.lstReturn);
+  }
+  /*
+  getRecommendedByGender(): Observable<any> {
+    let lstGenre = this.http.get<any>(`${this.baseUrl}/genre`);
+    console.log("getRecommendedByGender", lstGenre)
+    this.lstReturn = [];
+    /*for(let i = 0; i< lstGenre.length; i++){
+      this.lstReturn.push(this.http.get<any>(`${this.baseUrl}/genre`))
+    }*=/
+    return this.lstReturn;
+  }*/
   
 }
